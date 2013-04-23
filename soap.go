faf8fdd5b1495619f971d6f5ccaef6e9dd08b9ff
Jonathan Gilbert <bb..jgilbert@xoxy.net>
2013-04-23 21:38:35 +1000

    bugfixes and MarkLastPhotoInRoll

--- soap.go
+++ soap.go
@@ -8,58 +8,70 @@ import (
 
 const (
 	soapStart = `<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="EyeFi/SOAP/EyeFilm"><SOAP-ENV:Body>`
-	soapEnd = `</SOAP-ENV:Body></SOAP-ENV:Envelope>`
+	soapEnd   = `</SOAP-ENV:Body></SOAP-ENV:Envelope>`
 )
 
 type SoapStartSession struct {
-	MacAddress string `xml:"macaddress"`
-	CNonce     string `xml:"cnonce"`
+	MacAddress            string `xml:"macaddress"`
+	CNonce                string `xml:"cnonce"`
 	TransferMode          string `xml:"transfermode"`
 	TransferModeTimestamp string `xml:"transfermodetimestamp"`
 }
 
 type SoapStartSessionResponse struct {
-	Credential string `xml:"credential"`
-	SNonce     string `xml:"snonce"`
+	Credential            string `xml:"credential"`
+	SNonce                string `xml:"snonce"`
 	TransferMode          string `xml:"transfermode"`
 	TransferModeTimestamp string `xml:"transfermodetimestamp"`
-	UpSyncAllowed string `xml:"upsyncallowed"`
-	XMLName xml.Name `xml:"ns1:StartSessionResponse"`
+	UpSyncAllowed         string `xml:"upsyncallowed"`
+	XMLName               xml.Name `xml:"ns1:StartSessionResponse"`
 }
 
 type GetPhotoStatus struct {
-	Credential string `xml:"credential"`
-	MacAddress string `xml:"macaddress"`
-	Filename string `xml:"filename"`
-	FileSize string `xml:"filesize"`
+	Credential    string `xml:"credential"`
+	MacAddress    string `xml:"macaddress"`
+	Filename      string `xml:"filename"`
+	FileSize      string `xml:"filesize"`
 	FileSignature string `xml:"filesignature"`
-	Flags string `xml:"flags"`
-	XMLName xml.Name `xml:"ns1:GetPhotoStatus"`
+	Flags         string `xml:"flags"`
 }
 
 type GetPhotoStatusResponse struct {
-	FileID string `xml:"fileid"`
-	Offset string `xml:"offset"`
+	FileID  string `xml:"fileid"`
+	Offset  string `xml:"offset"`
 	XMLName xml.Name `xml:"ns1:GetPhotoStatusResponse"`
 }
 
 type UploadPhoto struct {
-	FileID string `xml:"fileid"`
-	MacAddress string `xml:"macaddress"`
-	Filename string `xml:"filename"`
-	FileSize string `xml:"filesize"`
+	FileID        string `xml:"fileid"`
+	MacAddress    string `xml:"macaddress"`
+	Filename      string `xml:"filename"`
+	FileSize      string `xml:"filesize"`
 	FileSignature string `xml:"filesignature"`
-	Encryption string `xml:"encryption"`
-	Flags string `xml:"flags"`
-	XMLName xml.Name `xml:"ns1:UploadPhoto"`
+	Encryption    string `xml:"encryption"`
+	Flags         string `xml:"flags"`
+}
+
+type UploadPhotoResponse struct {
+	Success string `xml:"success"`
+	XMLName xml.Name `xml:"ns1:UploadPhotoResponse"`
+}
+
+type MarkLastPhotoInRoll struct {
+	MacAddress string `xml:"macaddress"`
+	MergeDelta string `xml:"mergedelta"`
+}
+
+type MarkLastPhotoInRollResponse struct {
+	XMLName    xml.Name `xml:"ns1:MarkLastPhotoInRollResponse"`
 }
 
 func ParseSoap(s string, target interface {}) {
-	if s[0:len(soapStart)] != soapStart || s[len(s)-len(soapEnd):len(s)] != soapEnd {
+	if s[0:len(soapStart)] != soapStart || s[len(s) - len(soapEnd):len(s)] != soapEnd {
 		panic("Unknown soap request:\n" + s)
 	}
 
-	body := s[len(soapStart):len(s)-len(soapEnd)]
+	body := s[len(soapStart):len(s) - len(soapEnd)]
 
 	parser := xml.NewDecoder(bytes.NewBufferString(body))
 	parser.Decode(target)
