41f8150c2a7022d51eb584b743655eef6722a1e2
Jonathan Gilbert <bb..jgilbert@xoxy.net>
2013-05-27 22:32:44 +1000

    removed test code

--- eyego/main.go
+++ eyego/main.go
@@ -30,30 +30,10 @@ func main() {
 
 	eyego.Init(logfile, eyego.LogLevelFromName(*loglevelString))
 	eyego.ConfigFrom(*conf)
-//test()
 	http.HandleFunc("/", eyego.Handler)
 	eyego.Info("Server starting...")
 	err = http.ListenAndServe(":59278", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-}
-
-func test() {
-	soapString := `<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="EyeFi/SOAP/EyeFilm"><SOAP-ENV:Body><ns1:UploadPhoto><fileid>1</fileid><macaddress>001856433ca9</macaddress><filename>P1050237.JPG.tar</filename><filesize>5599232</filesize><filesignature>353100003000040000000000e0110300</filesignature><encryption>none</encryption><flags>4</flags></ns1:UploadPhoto></SOAP-ENV:Body></SOAP-ENV:Envelope>`
-	soap := new(eyego.UploadPhoto)
-	eyego.ParseSoap(soapString, soap)
-
-	eyego.Info("Uploading %s", soap.Filename)
-
-	soap.MacAddress = "001856433ca9"
-	soap.Filename = "P1050237.JPG.tar"
-	soap.FileSize = "5599232"
-	soap.FileSignature = "353100003000040000000000e0110300"
-	soap.Encryption = "none"
-	soap.Flags = "4"
-	panic(eyego.CreateSoap(soap))
-
-	panic("ok")
-
 }
